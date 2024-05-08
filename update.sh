204cab140bf96a03868ff29d631afcbccfac1ad2
torkus <48790775+torkus@users.noreply.github.com>
2024-05-08 10:28:06 +0930

    dump release.json files (#1)
    * manage.sh, adds 'deps.update' and 'test' commands
    * bumps deps
    * adds support for subcommands
    * adds subcommand 'dump-release-dot-json'
      it prints a list of release.json files to stdout, one per line
      output can be fed to release.json-validator

--- update.sh
+++ update.sh
@@ -4,6 +4,7 @@ mkdir -p output
 curl -s https://raw.githubusercontent.com/ogri-la/github-wow-addon-catalogue/develop/addons.csv > previous-addons.csv
 curl -s https://raw.githubusercontent.com/layday/github-wow-addon-catalogue/main/addons.csv > layday-addons.csv
 ADDONS_CATALOGUE_GITHUB_TOKEN=$(cat github-token) go run . \
+    scrape \
     --in previous-addons.csv \
     --in layday-addons.csv \
     --out addons.csv \
