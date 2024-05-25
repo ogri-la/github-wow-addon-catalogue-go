f4c44cd41153a80cc0558e54e23bab6cba078cb7
Torkus <48141663+ogri-la@users.noreply.github.com>
2024-05-25 09:43:05 +0930

    update.sh, adds imports.json to list of addons to use

--- update.sh
+++ update.sh
@@ -5,6 +5,7 @@ curl -s https://raw.githubusercontent.com/ogri-la/github-wow-addon-catalogue/dev
 curl -s https://raw.githubusercontent.com/layday/github-wow-addon-catalogue/main/addons.csv > layday-addons.csv
 ADDONS_CATALOGUE_GITHUB_TOKEN=$(cat github-token) go run . \
     scrape \
+    --in imports.json \
     --in previous-addons.csv \
     --in layday-addons.csv \
     --out addons.csv \
